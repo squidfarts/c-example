05cb8ac84c58cfae42899dac1a7426a3ac5a28aa
Micheal Brockus <42789598+squidfarts@users.noreply.github.com>
2019-07-23 16:45:08 -0700

    Add files via upload

---
+++ src/main/main.c
@@ -0,0 +1,29 @@
+/***********************************************************************************
+ *                                                                                 *
+ * NAME: main.c                                                                    *
+ *                                                                                 *
+ * AUTHOR: Michael Brockus.                                                        *
+ *                                                                                 *
+ * CONTACT: <mailto:michael@squidfarts.com>                                        *
+ *                                                                                 *
+ * NOTICES:                                                                        *
+ *                                                                                 *
+ * License: MIT                                                                    *
+ *                                                                                 *
+ ***********************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+/*
+ * This demonstrates a simple hello world program where the message
+ * 'Hello, c' is desplayed to the standerd console.
+ *
+ */
+int main(void)
+{
+    puts("Hello, c.");
+    
+    return EXIT_SUCCESS;
+}// end of function main
